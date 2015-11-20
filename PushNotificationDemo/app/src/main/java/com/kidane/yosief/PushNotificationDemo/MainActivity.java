947a3ddebb7e00e923e527d76e0913383af768fb
KidaneYosief <kidaneokbu@gmail.com>
2015-11-19 21:49:32 -0500

    PushNotification

--- PushNotificationDemo/app/src/main/java/com/kidane/yosief/PushNotificationDemo/MainActivity.java
+++ PushNotificationDemo/app/src/main/java/com/kidane/yosief/PushNotificationDemo/MainActivity.java
@@ -2,7 +2,6 @@ package com.kidane.yosief.PushNotificationDemo;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,9 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
-import com.kidane.yosief.PushNotificationDemo.R;
 import com.parse.Parse;
 import com.parse.ParseInstallation;
 
